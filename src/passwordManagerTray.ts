cb7820388a667bc4e934b0d51a47c164e9b2481a
Matsuda Yoshio <dark.unicorn1213@gmail.com>
2024-12-29 22:28:24 +0900

    feat: context menuを追加

--- src/passwordManagerTray.ts
+++ src/passwordManagerTray.ts
@@ -1,8 +1,14 @@
-import { app, BrowserWindow, Tray } from "electron";
+import { app, BrowserWindow, Tray, Menu } from "electron";
 import path from "path";
 
 class passwordManagerTray extends Tray {
   static readonly ICON_PATH: string = path.join(app.getAppPath(), "src/assets", "tray_image.ico");
+  static readonly MENU_CONFIG = Menu.buildFromTemplate([
+    {
+      label: "Exit",
+      click: () => app.quit(),
+    },
+  ]);
 
   private mainWindow: BrowserWindow;
 
@@ -13,9 +19,11 @@ class passwordManagerTray extends Tray {
     this.setToolTip("Password Manager");
 
     this.on("click", this.onClick);
+    this.on("right-click", this.onRightClick.bind(this));
   }
 
   onClick = () => (this.mainWindow.isVisible() ? this.mainWindow.hide() : this.mainWindow.show());
+  onRightClick = () => this.popUpContextMenu(passwordManagerTray.MENU_CONFIG);
 }
 
 export default passwordManagerTray;
