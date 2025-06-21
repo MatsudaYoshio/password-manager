11f7fb3a41e90c3e4688db6f51045383790f12e8
Matsuda Yoshio <dark.unicorn1213@gmail.com>
2025-06-21 22:02:20 +0900

    fix: アイコン画像を変更

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,7 +1,7 @@
 import { app } from "electron";
 import path from "path";
 
-export const ICON_PATH = path.join(app.getAppPath(), "src", "electron", "assets", "key.ico");
+export const ICON_PATH = path.join(app.getAppPath(), "src", "electron", "assets", "Aegis.ico");
 
 export const STORE_KEY_TREE_VIEW_EXPANDED_ITEMS = "treeViewExpandedItems";
 export const STORE_KEY_TREE_VIEW_SELECTED_ITEM_ID = "treeViewSelectedItemId";
