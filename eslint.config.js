5f460e530b77135af56cfb30d01de095a657f3ea
Matsuda Yoshio <dark.unicorn1213@gmail.com>
2025-08-02 10:38:45 +0900

    fix: huskyを追加

--- eslint.config.js
+++ eslint.config.js
@@ -44,6 +44,7 @@ module.exports = [
       'prettier/prettier': 'error',
       'react/react-in-jsx-scope': 'off',
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // Ignore unused arguments starting with _
+      '@typescript-eslint/no-explicit-any': 'warn'
     },
     settings: {
       react: { version: 'detect' },
