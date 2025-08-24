a653f16bf119be74c8ba0f2c8505a3533d45ba25
Matsuda Yoshio <dark.unicorn1213@gmail.com>
2025-08-24 10:28:40 +0900

    refactor: Extract common ESLint configuration to reduce duplication
    - Extract common languageOptions, globals, plugins, rules, and settings into shared variables
    - Reuse common configuration blocks in both TypeScript and test file configurations
    - Improve maintainability by eliminating code duplication
    - Changes to common rules now only need to be made in one place

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,54 @@ const reactPlugin = require('eslint-plugin-react');
 const reactHooks = require('eslint-plugin-react-hooks');
 const prettierPlugin = require('eslint-plugin-prettier');
 
+// 共通の設定
+const commonLanguageOptions = {
+  parser: tsParser,
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+    ecmaVersion: 2021,
+  },
+};
+
+const commonGlobals = {
+  api: true,
+  window: true,
+  document: true,
+  __dirname: true,
+  structuredClone: true,
+  React: 'readonly',
+  navigator: true,
+  process: true,
+  console: true,
+  BufferEncoding: true,
+  Electron: true,
+};
+
+const commonPlugins = {
+  '@typescript-eslint': tsPlugin,
+  react: reactPlugin,
+  'react-hooks': reactHooks,
+  prettier: prettierPlugin,
+};
+
+const commonRules = {
+  ...tsPlugin.configs.recommended.rules,
+  'prettier/prettier': 'error',
+  'react/react-in-jsx-scope': 'off',
+  '@typescript-eslint/no-unused-vars': [
+    'warn',
+    { argsIgnorePattern: '^_' },
+  ],
+  'react-hooks/rules-of-hooks': 'error',
+  'react-hooks/exhaustive-deps': 'warn',
+  '@typescript-eslint/no-unused-expressions': ['error', { allowTernary: true }]
+};
+
+const commonSettings = {
+  react: { version: 'detect' },
+};
+
 module.exports = [
   {
     ignores: ['node_modules', 'dist', 'release'],
@@ -13,47 +61,12 @@ module.exports = [
   {
     files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        project: './tsconfig.json',
-        sourceType: 'module',
-        ecmaVersion: 2021,
-      },
-      globals: {
-        api: true,
-        window: true,
-        document: true,
-        __dirname: true,
-        structuredClone: true,
-        React: 'readonly',
-        navigator: true,
-        process: true,
-        console: true,
-        BufferEncoding: true,
-        Electron: true,
-      },
+      ...commonLanguageOptions,
+      globals: commonGlobals,
     },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-      react: reactPlugin,
-      'react-hooks': reactHooks,
-      prettier: prettierPlugin,
-    },
-    rules: {
-      ...tsPlugin.configs.recommended.rules,
-      'prettier/prettier': 'error',
-      'react/react-in-jsx-scope': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        { argsIgnorePattern: '^_' }, // Ignore unused arguments starting with _
-      ],
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      '@typescript-eslint/no-unused-expressions': ['error', { allowTernary: true }]
-    },
-    settings: {
-      react: { version: 'detect' },
-    },
+    plugins: commonPlugins,
+    rules: commonRules,
+    settings: commonSettings,
   },
   // Jest/テストファイル用の設定
   {
@@ -65,12 +78,7 @@ module.exports = [
       'src/test/**/*.ts'
     ],
     languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        project: './tsconfig.json',
-        sourceType: 'module',
-        ecmaVersion: 2021,
-      },
+      ...commonLanguageOptions,
       globals: {
         // Jest globals
         jest: 'readonly',
@@ -82,41 +90,13 @@ module.exports = [
         beforeEach: 'readonly',
         afterAll: 'readonly',
         afterEach: 'readonly',
-        // Node/Browser globals
-        api: true,
-        window: true,
-        document: true,
-        __dirname: true,
-        structuredClone: true,
-        React: 'readonly',
-        navigator: true,
-        process: true,
-        console: true,
-        BufferEncoding: true,
-        Electron: true,
+        // 共通のグローバル変数を継承
+        ...commonGlobals,
       },
     },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-      react: reactPlugin,
-      'react-hooks': reactHooks,
-      prettier: prettierPlugin,
-    },
-    rules: {
-      ...tsPlugin.configs.recommended.rules,
-      'prettier/prettier': 'error',
-      'react/react-in-jsx-scope': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        { argsIgnorePattern: '^_' },
-      ],
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      '@typescript-eslint/no-unused-expressions': ['error', { allowTernary: true }]
-    },
-    settings: {
-      react: { version: 'detect' },
-    },
+    plugins: commonPlugins,
+    rules: commonRules,
+    settings: commonSettings,
   },
   // Node.js設定ファイル用の設定
   {
