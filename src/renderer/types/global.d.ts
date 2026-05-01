ede1f920b7b552ae1d05d9824966b5030c8ecf84
Matsuda Yoshio <dark.unicorn1213@gmail.com>
2026-05-01 13:21:08 +0900

    feat: add global window.api type definition and update moduleResolution to Bundler

--- src/renderer/types/global.d.ts
+++ src/renderer/types/global.d.ts
@@ -1,5 +1,8 @@
 import { ElectronAPI } from '../../shared/types/ElectronAPI';
 
+declare module '*.css';
+declare module 'normalize.css';
+
 declare global {
   interface Window {
     api: ElectronAPI;
