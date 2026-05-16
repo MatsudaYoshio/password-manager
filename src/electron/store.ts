51a2610bdfcbebd384632f3f7ff71d250e246311
Matsuda Yoshio <dark.unicorn1213@gmail.com>
2026-05-16 22:39:36 +0900

    fix: change dev config.json save location to separate from prod
    - src/electron/store.ts: Changed electron-store cwd to use a specific directory in development environment to avoid overwriting production config.json

--- src/electron/store.ts
+++ src/electron/store.ts
@@ -1,5 +1,13 @@
+import { app } from 'electron';
 import Store from 'electron-store';
+import path from 'path';
+import { isDevelopment } from './utils/environment';
 
-const store = new Store();
+const store = new Store({
+  // 開発時のみ '-dev'をつけたディレクトリに config.json を保存する
+  cwd: isDevelopment()
+    ? path.join(app.getPath('appData'), `${app.getName()}-dev`)
+    : app.getPath('userData')
+});
 
 export default store;
